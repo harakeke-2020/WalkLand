ff64b11aa6b0abc582a2199703a09c9ea2a2aa27
James Panaho <24538454+jpcentral@users.noreply.github.com>
2020-02-27 15:43:47 +1300

    import fixed

--- client/components/SideBarItem.jsx
+++ client/components/SideBarItem.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { activePage } from './actions/activePage'
+import activePage from './actions/activePage'
 
 class SideBarItem extends Component {
   render () {
