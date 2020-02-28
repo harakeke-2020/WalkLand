b41a12bbadde15e28103d30870a62b1b2378c3be
James Panaho <24538454+jpcentral@users.noreply.github.com>
2020-02-29 09:38:45 +1300

    commit before changing branch

--- client/components/SideBar.jsx
+++ client/components/SideBar.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import SideBarItem from './SideBarItem'
-// import mockData from '../../data/data'
+import mockData from '../../data/data'
 import { connect } from 'react-redux'
 import { fetchWalks } from './actions/allWalks'
 
 class SideBar extends Component {
   // state = { mockData }
   state = {
-    arr: []
+    arr: mockData
   }
   componentDidMount () {
     this.props.dispatch(fetchWalks())
       .then(res => {
-        this.setState({
-          arr: res })
+        console.log(res)
+      //   this.setState({
+      //     arr: res })
       })
   }
 
