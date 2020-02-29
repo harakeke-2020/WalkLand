d74461f1803c68715ed5702436cf3e6b9ea75b0d
James Panaho <24538454+jpcentral@users.noreply.github.com>
2020-03-01 12:37:30 +1300

    moved marker to island

--- seeds/02-walks.js
+++ seeds/02-walks.js
@@ -12,12 +12,12 @@ exports.seed = function (knex) {
           location: 'Muriwai',
           mainPhoto: 'https://images.unsplash.com/photo-1517896470636-9242d3a38392?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1268&q=80',
           photos: JSON.stringify([
-            'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
-            'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+            'https://tramper.nz/imgcache/8/8763-feature.jpg',
+            'https://www.nstc.org.nz/resources/Pictures/Te%20Henga1.jpg',
             'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1440&q=80',
             'https://images.unsplash.com/photo-1504700610630-ac6aba3536d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
           ]),
-          routeImage: 'https://i1.wp.com/exploringkiwis.com/wp-content/uploads/2019/04/Screen-Shot-2019-04-14-at-8.38.47-PM.png?resize=1393%2C800&ssl=1',
+          routeImage: 'https://basemaps.cloud.eaglegis.co.nz/arcgis/rest/services/LINZ/geotiffs/MapServer/tile/9/5754/2350',
           distance: 3.00,
           elevationGain: 200,
           timeTaken: 12352,
@@ -101,8 +101,8 @@ exports.seed = function (knex) {
         {
           id: 7,
           title: 'Motutapu Walkway',
-          latitude: -36.763222,
-          longitude: 174.8802163,
+          latitude: -36.749997,
+          longitude: 174.916663,
           location: 'Devonport',
           mainPhoto: 'https://www.doc.govt.nz/contentassets/7739db54ace849c5bbc7f397c5cf098c/north-head-interp-map-cropped-525.jpg',
           photos: JSON.stringify([
