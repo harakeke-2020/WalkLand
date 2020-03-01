576a704cee617450afd171f79b841a6d2eca09e4
Jared James <57459274+Jared-James@users.noreply.github.com>
2020-03-01 18:20:02 +1300

    trying carousel

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+//import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
 
 class Details extends Component {
   render () {
     const { selectedWalk } = this.props
+
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
     return (
-      <div>
-        <h3>{selectedWalk.title}</h3>
-        <h5>{selectedWalk.location}</h5>
-        <ul>
-          {
-            selectedWalk.photos.map((item, idx) => (<img key={idx} src={item} height="200" width="300" />))
-          }
-        </ul>
-        <img src={selectedWalk.routeImage} height="200" width="300" />
-        <ul>
+      <div className="details-container">
+        <h1 className = "details-walktitle">{selectedWalk.title}</h1>
+        <div className = "details-photo-slider">
+          <div className = "details-photos">
+            <Slider {...settings}>
+              {
+                selectedWalk.photos.map((item, idx) => (
+                  <img key={idx} src={item} />
+                ))
+              }
+            </Slider>
+          </div>
+        </div>
+        <button id ="prevBtn">Prev</button>
+        <button id ="nextBtn">Next</button>
+        <img className = "details-map" src={selectedWalk.routeImage} height="200" width="300" />
+        <ul className = "details-info">
+          <li>{`Location: ${selectedWalk.location}`}</li>
           <li>{`Distance: ${selectedWalk.distance}km`}</li>
           <li>{`Elevation Gain: ${selectedWalk.elevationGain}m`}</li>
           <li>{`Estimated Time: ${selectedWalk.timeTaken}`}</li>
