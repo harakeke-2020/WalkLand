4fa6a9293477f96da24b2b0d92fedc324d290dcb
Jared James <57459274+Jared-James@users.noreply.github.com>
2020-03-01 19:03:25 +1300

    logout button component created

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,6 +9,8 @@ import Details from './Details'
 import LogoComponent from './LogoComponent'
 import NavBar from './NavBar'
 import RegisterUser from './RegisterUser'
+import LoginUser from './LoginUser'
+import Logout from './Logout'
 
 class App extends Component {
   componentDidMount () {
@@ -24,6 +26,8 @@ class App extends Component {
           </div>
         }
         <RegisterUser />
+        <LoginUser />
+        <Logout />
         {this.props.activePage === 'map' &&
           <div className="map-page-container">
             <div className="logo-container">
--- client/components/reducers/index.js
+++ client/components/reducers/index.js
@@ -3,11 +3,11 @@ import { combineReducers } from 'redux'
 import activePage from './activePage'
 import selectedWalk from './selectedWalk'
 import allWalks from './allWalks'
-import login from './login'
+import auth from './auth'
 
 export default combineReducers({
   activePage,
   selectedWalk,
   allWalks,
-  login
+  auth
 })
