386f36fc6eb3bc0f654fe71ba4f533819b2888ad
Tom <thomas.mathers@outlook.com>
2020-03-02 01:12:16 +1300

    test

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -1,32 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import Slider from 'react-slick'
+import Carousel from 'nuka-carousel';
+
 
 class Details extends Component {
+  
   render () {
     const { selectedWalk } = this.props
 
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    }
+    console.log("hello")
+    // const settings = {
+    //   dots: true,
+    //   infinite: true,
+    //   speed: 500,
+    //   slidesToShow: 1,
+    //   slidesToScroll: 1
+    // }
     return (
       <div className="details-container">
         <h1 className = "details-walktitle">{selectedWalk.title}</h1>
         <div className = "details-photo-slider">
           <div className = "details-photos">
-            <Slider {...settings}>
+          <Carousel>
+            
               {
                 selectedWalk.photos.map((item, idx) => (
                   <img key={idx} src={item} />
                 ))
               }
-            </Slider>
+             </Carousel>
           </div>
         </div>
         <button id ="prevBtn">Prev</button>
