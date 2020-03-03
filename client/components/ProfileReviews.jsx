a1c0a8d76c171480622c2ced47949ef13e69f450
Kevin Kim <hwaksiliya@hotmail.com>
2020-03-04 12:04:39 +1300

    component only shows if there are 3 reviews

--- client/components/ProfileReviews.jsx
+++ client/components/ProfileReviews.jsx
@@ -2,55 +2,69 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class ProfileReviews extends Component {
+  
   render() {
     let reviewsArr = []
-
     let reviews = this.props.ratings.map(ratingInfo => {
       if (ratingInfo.username === this.props.currentUsername && !this.props.otherUsername.isViewing) {
+        reviewsArr.push(<div>{ratingInfo.walkId}</div>)
         reviewsArr.push(<div>{ratingInfo.review}</div>)
         reviewsArr.push(<div>{ratingInfo.rating} COOKIES!!</div>)
-
+        
         // return <div>{ratingInfo.review}</div>
       } else if (ratingInfo.username === this.props.otherUsername.username && this.props.otherUsername.isViewing) {
+        reviewsArr.push(<div>{ratingInfo.walkId}</div>)
         reviewsArr.push(<div>{ratingInfo.review}</div>)
         reviewsArr.push(<div>{ratingInfo.rating} COOKIES!!</div>)
         // return <div>{ratingInfo.review}</div>
       }
     })
-
+    
     return (
-      <>
-        <div className="profile-bot-container-left">
-          <div className="profile-bot-content-card">
-            My review: {reviewsArr[0]} <br/>
-            My rating: {reviewsArr[1]}
-          </div>
-        </div>
-
-        <div className="profile-bot-container-mid">
-          <div className="profile-bot-content-card">
-            My review: {reviewsArr[2]} <br/>
-            My rating: {reviewsArr[3]}
-          </div>
-        </div>
-
-        <div className="profile-bot-container-right">
-          <div className="profile-bot-content-card">
-            {reviewsArr[4]}
-            {reviewsArr[5]}
-          </div>
-        </div>
-      </>
-    )
+      <div>
+      {console.log('reviewsArr: ', reviewsArr)}
+      <div className="profile-bot-container-left">
+      <div className="profile-bot-content-card">
+      Walk name: {this.props.allWalks.find(walk => walk.id === reviewsArr[0].props.children).title} <br/>
+      My review: {reviewsArr[1]} <br/>
+      My rating: {reviewsArr[2]}
+      </div>
+      </div>
+      
+      <div className="profile-bot-container-mid">
+      <div className="profile-bot-content-card">
+      Walk name: {this.props.allWalks.find(walk => walk.id === reviewsArr[3].props.children).title} <br/>
+      My review: {reviewsArr[4]} <br/>
+      My rating: {reviewsArr[5]}
+      </div>
+      </div>
+      
+      <div className="profile-bot-container-right">
+      <div className="profile-bot-content-card">
+      Walk name: {this.props.allWalks.find(walk => walk.id === reviewsArr[6].props.children).title} <br/>
+      My review: {reviewsArr[7]} <br/>
+      My rating: {reviewsArr[8]}
+      </div>
+      </div>
+      </div>
+      )
+    }
   }
-}
 
-const mapStateToProps = state => {
-  return {
-    ratings: state.ratings,
-    currentUsername: state.auth,
-    otherUsername: state.viewProfile
+  // const userId = !!this.props.userId.id && this.props.userId.id
+  // const allRatings = this.props.ratings
+  // const allWalks = this.props.allWalks
+  // const reviewByUser = allRatings.filter(rating => rating.userId === userId)
+  
+  const mapStateToProps = state => {
+    return {
+      ratings: state.ratings,
+      currentUsername: state.auth,
+      otherUsername: state.viewProfile,
+      userId: state.userProfiles,
+      allWalks: state.allWalks
+      
+    }
   }
-}
 
 export default connect(mapStateToProps)(ProfileReviews)
