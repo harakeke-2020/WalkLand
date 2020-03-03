d9d190ac20c8e15ce49c88a3b3528d5d253cc249
emily135shen <emily135shen@gmail.com>
2020-03-03 17:28:13 +1300

    change home page styling

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -5,19 +5,17 @@ import activePage from './actions/activePage'
 export const Landing = (props) => {
   return (
     <>
-      <div data-testid="fullscreen" className="fullscreen-bg">
-        <video autoPlay muted loop id="myVideo" className="fullscreen-bg__video">
-          <source data-testid="video" src='landingvideo1.mp4' type="video/mp4" />
-        </video>
 
+      <div data-testid="fullscreen" className="fullscreen-bg">
+      
       </div>
-      <h1 data-testid="title" className="landing-heading-centre">Walkland</h1>
+      <div className="logo">
+      <img src="images/Logowalkland white.png"></img>
+      </div>
       <div className="btn-group-wrap">
-        <button data-testid="startButton" className="btn" onClick={() => props.activePage('map')}>START JOURNEY</button>
+        <button data-testid="startButton" className="btn btn-primary btn-sm" onClick={() => props.activePage('map')}>START JOURNEY</button>
       </div>
-      <div>
-        <button data-testid="startButton" onClick={() => props.activePage('profile')}>Login</button>
-      </div>
+
     </>
   )
 }
