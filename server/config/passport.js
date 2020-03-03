0dbb0ccfa1ae5c41da7a925698c394b1a7f71d6f
Hamish Stobo <51815749+HHSNZ@users.noreply.github.com>
2020-03-04 07:35:20 +1300

    delete email field from state in register form. Delete unused comments in passport config and auth routes.

--- server/config/passport.js
+++ server/config/passport.js
@@ -78,47 +78,7 @@ passport.use(
   )
 )
 
-// this route persists a login if the app is exited and restarted
-
-// router.get('/findUser', (req, res, next) => {
-//   passport.authenticate('jwt', { session: false }, (err, user, info) => {
-//     if (err) {
-//       console.log(err)
-//     }
-//     if (info !== undefined) {
-//       console.log(info.message)
-//       res.status(401).send(info.message)
-//     } else if (user.username === req.query.username) {
-//       User.findOne({
-//         where: {
-//           username: req.query.username
-//         }
-//       }).then((userInfo) => {
-//         if (userInfo != null) {
-//           console.log('user found in db from findUsers')
-//           res.status(200).send({
-//             auth: true,
-//             first_name: userInfo.first_name,
-//             last_name: userInfo.last_name,
-//             email: userInfo.email,
-//             username: userInfo.username,
-//             password: userInfo.password,
-//             message: 'user found in db'
-//           })
-//         } else {
-//           console.error('no user exists in db with that username')
-//           res.status(401).send('no user exists in db with that username')
-//         }
-//       })
-//     } else {
-//       console.error('jwt id and username do not match')
-//       res.status(403).send('username and jwt token do not match')
-//     }
-//   })(req, res, next)
-// })
-
 const opts = {
-  // jwtFromRequest: ExtractJWT.fromAuthHeaderWithScheme('JWT'),
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.SECRET_KEY
 }
@@ -132,7 +92,7 @@ passport.use(
       db.findUserJWT(jwtPayload.id)
         .then(user => {
           if (user) {
-            console.log('user is authorized for next action ', user)
+            console.log('user is authorized for next action ', user.username)
             done(null, user)
           } else {
             console.log('user not found in db')
