3f96d7b8577377bd59dcba1d2e254708ab1cc9f1
Jinsang Kim <57661979+kevin-jinsang-kim@users.noreply.github.com>
2020-03-04 19:05:11 +1300

    css styling side bar

--- client/components/SideBarItem.jsx
+++ client/components/SideBarItem.jsx
@@ -7,7 +7,7 @@ import { getReviewRatings } from './actions/allWalks'
 class SideBarItem extends Component {
   render () {
     const walk = this.props.walk
-    const { id, title, mainPhoto } = walk
+    const { id, title, mainPhoto, distance } = walk
     const { activePageState, selectedWalkState } = this.props
     const { selectedWalk, activePage } = this.props
     const walkRatings = this.props.ratings // array of ratings
@@ -16,11 +16,14 @@ class SideBarItem extends Component {
     const walkAverage = filterByWalk.reduce((total, next) => total + Number(next.rating), 0) / filterByWalk.length
 
     const style = {
-      backgroundImage: `url(${mainPhoto})`
+      backgroundImage: `url(${mainPhoto})`,
+      backgroundSize: '100% auto',
+      backgroundPosition: 'center',
+      opacity: '0.9'
     }
 
     return (
-      <div>
+      <div className='overlay'>
         <div style={style}
           className={
             `${selectedWalkState.id === id ? 'selected-walk' : 'sidebar-item'}`
@@ -29,8 +32,9 @@ class SideBarItem extends Component {
             selectedWalk(walk)
           }}
         >
-          <h2 data-testid={'sideBarTitle'}> {title} </h2>
-          {walkAverage ? <p data-testid={'rating'}>rating: {Math.round((walkAverage + Number.EPSILON) * 100) / 100}</p> : <p>No rating yet</p>}
+          <h2 className='item-title' data-testid={'sideBarTitle'}> {title} </h2>
+          {walkAverage ? <p className='sidebar-rating' data-testid={'rating'}>rating: {Math.round((walkAverage + Number.EPSILON) * 100) / 100}</p> : <p>No rating yet</p>}
+        <p className='sidebar-distance'>Distance: {distance}</p>
           { selectedWalkState.id === id &&
         <>
 
@@ -40,7 +44,6 @@ class SideBarItem extends Component {
         <button data-testid="showDetails" name = 'showDetials' onClick={() => activePage('details') }>Show Details</button>}
         </>
           }
-
         </div>
       </div>
     )
