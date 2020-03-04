85f817428271956ed9d3e01703c5be058935c49e
H-THO <hywel.p.thomas@gmail.com>
2020-03-04 13:17:22 +1300

    finished backend testing

--- tests/backend/seeds/00-clean.js
+++ tests/backend/seeds/00-clean.js
@@ -1,11 +1,10 @@
 exports.seed = function (knex, Promise) {
-    const empty = table =>
-      () => knex(table).del()
-  
-    return empty('users')()
-    .then(empty('walks'))
-    .then(empty('ratingReviews')
+  const empty = table =>
+    () => knex(table).del()
+
+  return empty('walks')()
+    .then(empty('ratingReviews'))
     // Chain calls to empty in
     // order as required, e.g.:
     // .then(empty('profiles'))
-  }
+}
--- tests/backend/seeds/01-users.js
+++ tests/backend/seeds/01-users.js
@@ -1,16 +1,13 @@
 exports.seed = function (knex) {
-    // Deletes ALL existing entries
-    return knex('users').del()
-      .then(function () {
-        // Inserts seed entries
-        return knex('users').insert([
-          { id: 1, username: 'testUserName1', email: 'testEmail1.com', password: 'test1' },
-          { id: 2, username: 'testUserName2', email: 'testEmail2.com', password: 'test2' },
-          { id: 3, username: 'testUserName3', email: 'testEmail3.com', password: 'test3' }
-        ])
-      })
-  }
-  
-  // on register
-  // bio
-  // user profile
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { username: 'testEmail1.com', password: 5 },
+        { username: 'testEmail2.com', password: 3 },
+        { username: 'testEmail3.com', password: 2 }
+
+      ])
+    })
+}
