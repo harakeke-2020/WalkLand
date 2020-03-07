63f93de9274d129dbd8a4daa1becc06d1c2aec8e
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-03-08 04:25:30 +1300

    fix login issue when deployed

--- seeds/03-ratingReviews.js
+++ seeds/03-ratingReviews.js
@@ -2,16 +2,16 @@ exports.seed = function (knex) {
   return knex('ratingreviews').del()
     .then(function () {
       return knex('ratingreviews').insert([
-        { id: 1, walkId: 1, username: 'Felix', rating: 5, review: 'Goldie\'s Bush walk was amazing' },
-        { id: 2, walkId: 2, username: 'Doderick', rating: 3, review: 'Western springs walk was amazing' },
-        { id: 3, walkId: 3, username: 'Mylanta', rating: 5, review: 'Te Henga Walkway was amazing. The entry into the beach, the walk along the cliffside, the ease into Muriwai Beach. It truly showed the best of the beauty that Auckland\'s West Coast has to offer.' },
-        { id: 4, walkId: 4, username: 'Duodenum', rating: 4, review: 'Rangitoto Summit Track was amazing' },
-        { id: 5, walkId: 5, username: 'Femur', rating: 4, review: 'Maungauika/North Head Historic Walk was amazing' },
-        { id: 6, walkId: 6, username: 'themonkey', rating: 2, review: 'Motutapu Walkway was amazing' },
-        { id: 7, walkId: 7, username: 'dave', rating: 5, review: 'One Tree Hill Walk was amazing' },
-        { id: 8, walkId: 8, username: 'bob', rating: 5, review: 'Kitekite Falls was amazing' },
-        { id: 9, walkId: 9, username: 'jimmy', rating: 2, review: 'Orakei Basin Walkway fast and easy!' },
-        { id: 10, walkId: 10, username: 'hamish', rating: 3, review: 'Long bay was awesome' }
+        {walkId: 1, username: 'Felix', rating: 5, review: 'Goldie\'s Bush walk was amazing' },
+        {  walkId: 2, username: 'Doderick', rating: 3, review: 'Western springs walk was amazing' },
+        {  walkId: 3, username: 'Mylanta', rating: 5, review: 'Te Henga Walkway was amazing. The entry into the beach, the walk along the cliffside, the ease into Muriwai Beach. It truly showed the best of the beauty that Auckland\'s West Coast has to offer.' },
+        {  walkId: 4, username: 'Duodenum', rating: 4, review: 'Rangitoto Summit Track was amazing' },
+        {  walkId: 5, username: 'Femur', rating: 4, review: 'Maungauika/North Head Historic Walk was amazing' },
+        {  walkId: 6, username: 'themonkey', rating: 2, review: 'Motutapu Walkway was amazing' },
+        {  walkId: 7, username: 'dave', rating: 5, review: 'One Tree Hill Walk was amazing' },
+        {  walkId: 8, username: 'bob', rating: 5, review: 'Kitekite Falls was amazing' },
+        {  walkId: 9, username: 'jimmy', rating: 2, review: 'Orakei Basin Walkway fast and easy!' },
+        { walkId: 10, username: 'hamish', rating: 3, review: 'Long bay was awesome' }
       ])
     })
 }
