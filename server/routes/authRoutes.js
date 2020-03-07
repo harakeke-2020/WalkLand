37428c125f555b9b1df67ec8683aa37d396ab27e
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-03-08 02:58:08 +1300

    fix login issue when deployed

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,97 +1,110 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express')
-const router = express.Router()
+const request = require("superagent");
+const express = require("express");
+const router = express.Router();
 
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const jwtSecret = process.env.SECRET_KEY
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const jwtSecret = process.env.SECRET_KEY;
+const jwt = require("jsonwebtoken");
 
-const db = require('../db/db')
+const db = require("../db/db");
 
-router.post('/registerUser', (req, res, next) => {
-  passport.authenticate('register', (err, user, info) => {
+router.post("/registerUser", (req, res, next) => {
+  return request
+    .post("http://localhost:3000/api/v1/auth/registerUser")
+    .then(() => {
+      passport.authenticate("register", (err, user, info) => {
+        if (err) {
+          console.error(err);
+        }
+        if (info !== undefined) {
+          res.statusMessage = `${info.message}`;
+          res.status(403).end();
+        } else {
+          res.statusMessage = "user created!";
+          res.status(200).end();
+        }
+      })(req, res, next);
+    });
+});
+
+router.post("/loginUser", (req, res, next) => {
+  return request
+    .post("http://localhost:3000/api/v1/auth/loginUser")
+    .then(() => {
+      passport.authenticate("login", (err, users, info) => {
+        if (err) {
+          console.error(`error ${err}`);
+        }
+        if (info !== undefined) {
+          console.log("login error ", info.message);
+          res.statusMessage = `${info.message}`;
+          if (info.message === "bad username") {
+            res.status(401).end();
+          } else {
+            res.status(403).end();
+          }
+        } else {
+          db.findUser(req.body.username).then(user => {
+            const token = jwt.sign({ id: user.id }, jwtSecret, {
+              expiresIn: 60 * 60
+            });
+            res.status(200).send({
+              auth: true,
+              token,
+              message: "user found & logged in"
+            });
+          });
+        }
+      })(req, res, next);
+    });
+});
+
+router.delete("/deleteUser/:username", (req, res, next) => {
+  console.log("request in route ", req.params);
+  passport.authenticate("jwt", { session: false }, (err, user, info) => {
     if (err) {
-      console.error(err)
+      console.error(err);
     }
     if (info !== undefined) {
-      res.statusMessage = `${info.message}`
-      res.status(403).end()
+      console.error(info.message);
+      res.statusMessage = `${info.message}`;
+      res.status(403).end();
     } else {
-      res.statusMessage = 'user created!'
-      res.status(200).end()
-    }
-  })(req, res, next)
-})
-
-router.post('/loginUser', (req, res, next) => {
-  passport.authenticate('login', (err, users, info) => {
-    if (err) {
-      console.error(`error ${err}`)
-    }
-    if (info !== undefined) {
-      console.log('login error ', info.message)
-      res.statusMessage = `${info.message}`
-      if (info.message === 'bad username') {
-        res.status(401).end()
-      } else {
-        res.status(403).end()
-      }
-    } else {
-      db.findUser(req.body.username)
-        .then(user => {
-          const token = jwt.sign({ id: user.id }, jwtSecret, {
-            expiresIn: 60 * 60
-          })
-          res.status(200).send({
-            auth: true,
-            token,
-            message: 'user found & logged in'
-          })
-        })
-    }
-  })(req, res, next)
-})
-
-router.delete('/deleteUser/:username', (req, res, next) => {
-  console.log('request in route ', req.params)
-  passport.authenticate('jwt', { session: false }, (err, user, info) => {
-    if (err) {
-      console.error(err)
-    }
-    if (info !== undefined) {
-      console.error(info.message)
-      res.statusMessage = `${info.message}`
-      res.status(403).end()
-    } else {
-      console.log('username in params ', req.params.username, 'username from jwt auth ', user.username)
+      console.log(
+        "username in params ",
+        req.params.username,
+        "username from jwt auth ",
+        user.username
+      );
       if (req.params.username === user.username) {
         db.deleteUser(user.username)
-          .then((userInfo) => {
+          .then(userInfo => {
             if (userInfo === 1) {
-              console.log('user deleted from db')
-              res.statusMessage = `${'user deleted from db'}`
-              res.status(200).end()
+              console.log("user deleted from db");
+              res.statusMessage = `${"user deleted from db"}`;
+              res.status(200).end();
             } else {
-              res.statusMessage = 'no user with that username to delete'
-              res.status(404).end()
+              res.statusMessage = "no user with that username to delete";
+              res.status(404).end();
             }
           })
-          .catch((error) => {
-            console.log(error)
-            res.statusMessage = 'problem communicating with db'
-            res.status(500).end()
-          })
+          .catch(error => {
+            console.log(error);
+            res.statusMessage = "problem communicating with db";
+            res.status(500).end();
+          });
       } else {
-        console.error('jwt id and username do not match')
-        res.statusMessage = 'You are not authorized'
-        res.status(403).end()
+        console.error("jwt id and username do not match");
+        res.statusMessage = "You are not authorized";
+        res.status(403).end();
       }
     }
-  })(req, res, next)
-})
+  })(req, res, next);
+});
 
-module.exports = router
+module.exports = router;
